7432ef00b5198e057b421aaab5ccf3be36b64631
AlphaGergedan <atamertiel@gmail.com>
2024-02-09 12:33:45 +0100

    use generator instead of seed in grid generation

--- src/utils/grid.py
+++ src/utils/grid.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def generate_grid(N_qs, N_ps, q_lims, p_lims, dof, linspace=False, random_seed=None):
+
+def generate_grid(N_qs, N_ps, q_lims, p_lims, dof, linspace=False, rng=None):
     """
     Generates meshgrid for Hamiltonian systems with positions q and momenta p
 
@@ -28,7 +29,7 @@ def generate_grid(N_qs, N_ps, q_lims, p_lims, dof, linspace=False, random_seed=N
             q_ranges.append(np.linspace(q_lims[d][0], q_lims[d][1], N_qs[d]))
             p_ranges.append(np.linspace(p_lims[d][0], p_lims[d][1], N_ps[d]))
     else:
-        rng = np.random.default_rng(random_seed)
+        assert rng != None
         for d in range(dof):
             q_ranges.append(rng.uniform(low=q_lims[d][0], high=q_lims[d][1], size=(N_qs[d])))
             p_ranges.append(rng.uniform(low=p_lims[d][0], high=p_lims[d][1], size=(N_ps[d])))
@@ -38,7 +39,7 @@ def generate_grid(N_qs, N_ps, q_lims, p_lims, dof, linspace=False, random_seed=N
     # returns q_grids, p_grids
     return q_ranges, p_ranges, grids[:dof], grids[dof:]
 
-def generate_train_test_grid(train_qs, train_ps, train_q_lims, train_p_lims, test_qs, test_ps, test_q_lims, test_p_lims, dof, linspace=False, random_seed=None):
+def generate_train_test_grid(train_qs, train_ps, train_q_lims, train_p_lims, test_qs, test_ps, test_q_lims, test_p_lims, test_rng, dof, linspace=False, train_rng=None):
     """
     Generates meshgrid for Hamiltonian systems with positions q and momenta p
     with uniform sampling from the given ranges. The test set does not include the training
@@ -65,10 +66,9 @@ def generate_train_test_grid(train_qs, train_ps, train_q_lims, train_p_lims, tes
         assert len(param) == dof
 
     # generate train set first
-    train_q_ranges, train_p_ranges, train_q_grid, train_p_grid = generate_grid(train_qs, train_ps, train_q_lims, train_p_lims, dof, linspace=linspace, random_seed=random_seed)
+    train_q_ranges, train_p_ranges, train_q_grid, train_p_grid = generate_grid(train_qs, train_ps, train_q_lims, train_p_lims, dof, linspace=linspace, rng=train_rng)
 
     # now generate distinct test set
-    rng = np.random.default_rng(random_seed)
     test_q_ranges = []
     test_p_ranges = []
 
@@ -80,13 +80,15 @@ def generate_train_test_grid(train_qs, train_ps, train_q_lims, train_p_lims, tes
     # sample distinct test samples
     for d in range(dof):
         while len(test_q_ranges[d]) < test_qs[d]:
-            candidate_samples = rng.uniform(low=train_q_lims[d][0], high=train_q_lims[d][1], size=(test_qs[d] - len(test_q_ranges[d])))
+            candidate_samples = test_rng.uniform(low=test_q_lims[d][0], high=test_q_lims[d][1], size=(test_qs[d] - len(test_q_ranges[d])))
+            # setdiff1d returns all elements in arr1 that are not in arr2
             new_samples = np.setdiff1d(candidate_samples, train_q_ranges[d])
             test_q_ranges[d].extend(new_samples)
         test_q_ranges[d] = np.asarray(test_q_ranges[d][:test_qs[d]])
 
         while len(test_p_ranges[d]) < test_ps[d]:
-            candidate_samples = rng.uniform(low=train_p_lims[d][0], high=train_p_lims[d][1], size=(test_ps[d] - len(test_p_ranges[d])))
+            candidate_samples = test_rng.uniform(low=test_p_lims[d][0], high=test_p_lims[d][1], size=(test_ps[d] - len(test_p_ranges[d])))
+            # setdiff1d returns all elements in arr1 that are not in arr2
             new_samples = np.setdiff1d(candidate_samples, train_p_ranges[d])
             test_p_ranges[d].extend(new_samples)
         test_p_ranges[d] = np.asarray(test_p_ranges[d][:test_ps[d]])
