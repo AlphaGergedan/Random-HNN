dd8d8de396e63d402e7c7336b664fa41553170d5
AlphaGergedan <atamertiel@gmail.com>
2024-02-10 17:44:40 +0100

    error functions removed from utils

--- src/utils/l2_error.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def l2_error(y_true, y_pred):
-    y_true = y_true.reshape(-1)
-    y_pred = y_pred.reshape(-1)
-    assert y_true.shape == y_pred.shape
-
-    return np.sqrt(np.sum((y_true - y_pred)**2))
