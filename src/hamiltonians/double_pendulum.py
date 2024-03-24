6f5e042170abf6533b0d1ff3037d8dbff61567c7
AlphaGergedan <atamertiel@gmail.com>
2024-03-25 00:55:08 +0100

    lotka and double pend parametrized

--- src/hamiltonians/double_pendulum.py
+++ src/hamiltonians/double_pendulum.py
@@ -1,12 +1,16 @@
 import numpy as np
 
 # see https://diego.assencio.com/?index=e5ac36fcb129ce95a61f8e8ce0572dbf for a good source
+# see cranmer-2020
+# see jakovac-2022
+# see chen-2021
+# see jin-2021
 
 class DoublePendulum():
     """
     2 dof
     """
-    def __init__(self):
+    def __init__(self, m1=1, m2=1, l1=1, l2=1, g=1):
         """
         TODO setup specific
         m1: Mass of first pendulum
@@ -17,7 +21,11 @@ class DoublePendulum():
         TODO damped nonlinear pendulum: L*THETA_dot_dot + b*THETA_dot + g*sin(THETA) = 0
         TODO forced damped nonlinear pendulum: L*THETA_dot_dot + b*THETA_dot + g*sin(THETA) = F*cos(wt)
         """
-        self.m1 = self.m2 = self.l1 = self.l2 = self.g = 1
+        self.m1 = m1
+        self.m2 = m2
+        self.l1 = l1
+        self.l2 = l2
+        self.g = g
 
     # Hamiltonian Formulations
 
@@ -40,7 +48,7 @@ class DoublePendulum():
         # output is array of values [y_1, y_2, ...] where y_i is a real number (1-dimensional)
         assert x.shape == (x.shape[0],4)
         (q1,q2,p1,p2) = x[:,0], x[:,1], x[:,2], x[:,3]
-        f = ( (p1**2 + 2 * p2**2 - 2 * p1 * p2 * np.cos(q1 - q2) ) / (2 * (1 + np.sin(q1 - q2)**2)) ) - 2 * np.cos(q1) - np.cos(q2)
+        f = ( (self.m2 * self.l2**2 * p1**2 + (self.m1 + self.m2) * self.l1**2 * p2**2 - 2 * self.m2 * self.l1 * self.l2 * p1 * p2 * np.cos(q1 - q2) ) / (2 * self.m2 * self.l1**2 * self.l2**2 * (self.m1 + self.m2 * np.sin(q1 - q2)**2)) ) - (self.m1 + self.m2) * self.g * self.l1 * np.cos(q1) - self.m2 * self.g * self.l2 * np.cos(q2)
         return f.reshape(-1, 1)
 
     def dH(self, x):
@@ -56,13 +64,13 @@ class DoublePendulum():
         (q1,q2,p1,p2) = x[:,0], x[:,1], x[:,2], x[:,3]
 
         # define helper equations used commonly in dH/dq1 and dH/dq2
-        h1 = (p1 * p2 * np.sin(q1 - q2)) / (1 + np.sin(q1 - q2)**2)
-        h2 = (p1**2 + 2 * p2**2 - 2 * p1 * p2 * np.cos(q1 - q2)) / (2 * (1 + np.sin(q1 - q2)**2)**2)
+        h1 = (p1 * p2 * np.sin(q1 - q2)) / (self.l1 * self.l2 * (self.m1 + self.m2 * np.sin(q1 - q2)**2))
+        h2 = (self.m2 * self.l2**2 * p1**2 + (self.m1 + self.m2) * self.l1**2 * p2**2 - 2 * self.m2 * self.l1 * self.l2 * p1 * p2 * np.cos(q1 - q2)) / (2 * self.l1**2 * self.l2**2 * (self.m1 + self.m2 * np.sin(q1 - q2)**2)**2)
 
-        dq1 = 2 * np.sin(q1) + h1 - h2 * np.sin(2 * (q1 - q2))
-        dq2 = np.sin(q2) - h1 + h2 * np.sin(2 * (q1 - q2))
-        dp1 = (p1 - p2 * np.cos(q1 - q2)) / (1 + np.sin(q1 - q2)**2)
-        dp2 = (2 * p2 - p1 * np.cos(q1 - q2)) / (1 + np.sin(q1 - q2)**2)
+        dq1 = (self.m1 + self.m2) * self.g * self.l1 * np.sin(q1) + h1 - h2 * np.sin(2 * (q1 - q2))
+        dq2 = self.m2 * self.g * self.l2 * np.sin(q2) - h1 + h2 * np.sin(2 * (q1 - q2))
+        dp1 = (self.l2 * p1 - self.l1 * p2 * np.cos(q1 - q2)) / (self.l1**2 * self.l2 * (self.m1 + self.m2 * np.sin(q1 - q2)**2))
+        dp2 = ((self.m1 + self.m2) * self.l1 * p2 - self.m2 * self.l2 * p1 * np.cos(q1 - q2)) / (self.m2 * self.l1 * self.l2**2 * (self.m1 + self.m2 * np.sin(q1 - q2)**2))
 
         # (x.shape[0], 4)
         df = np.array([ dq1, dq2, dp1, dp2 ]).T
--- src/hamiltonians/lotka_volterra.py
+++ src/hamiltonians/lotka_volterra.py
@@ -9,13 +9,30 @@ class LotkaVolterra():
 
     """
 
+    def __init__(self, alpha=-2, beta=-1, gamma=-1, delta=-1):
+        """
+        Choose these parameters wisely, they change the phase plot.
+        For the classical rabbit and fox model you can set
+        alpha=1.1, beta=0.4, gamma=0.1, delta=0.4
+
+        @param alpha: prey's: the maximum prey per capita growth rate
+        @param beta : prey's: the effect of the presence of predators on the prey death rate
+        @param gamma: predator's: predator's per capita death rate
+        @param delta: predator's: the effect of the presence of prey on the predator growth rate
+        """
+        self.delta = delta
+        self.gamma = gamma
+        self.beta = beta
+        self.alpha = alpha
+
     def H(self, x):
         """
         Hamiltonian system with 1 degree of freedom, which means input is (q,p)
         H(q,p) = p - e^p + 2q - e^q
         """
         (q,p) = x[:,0], x[:,1]
-        f = p - np.e**p + 2*q - np.e**q
+        # f = delta*e^p - gamma*p + beta*e^q - alpha*q
+        f = self.delta * np.e**p - self.gamma * p + self.beta * np.e**q - self.alpha * q
         return f.reshape(-1, 1)
 
     def dH(self, x):
@@ -23,7 +40,9 @@ class LotkaVolterra():
         Implementaion of the gradient of the Hamiltonian
         """
         (q,p) = x[:,0], x[:,1]
-        dq = 2 - np.e**q
-        dp = 1 - np.e**p
+        dq = self.beta * np.e**q - self.alpha
+        # dq = -alpha + beta*e^q
+        dp = self.delta * np.e**p - self.gamma
+        # dp = -gamma + delta*e^p
         df = np.array([dq, dp]).T
         return df.reshape(-1, 2)
