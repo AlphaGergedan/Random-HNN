8d09a52563071dc27617d84db0e88dd4351bcc3c
AlphaGergedan <atamertiel@gmail.com>
2024-03-24 16:48:48 +0100

    get median model from experiment
    also used index instead of error functions

---
+++ src/error_functions/index.py
@@ -0,0 +1,4 @@
+from .mae import mean_absolute_error
+from .mse import mean_squared_error
+from .l2_error import l2_error
+from .l2_error_relative import l2_error_relative
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,8 @@
 from activations import relu, tanh, sigmoid, elu, identity, gaussian, gelu, silu, softplus
 from hamiltonians import single_pendulum, lotka_volterra, double_pendulum, trigonometric
-from error_functions import mae, mse, l2_error, l2_error_relative
+from error_functions.index import mean_absolute_error, mean_squared_error, l2_error, l2_error_relative
 import numpy as np
+from joblib import load
 
 def parse_activation(activation):
     """
@@ -73,13 +74,13 @@ def parse_error_function(error_function_name):
     """
     match error_function_name:
         case "mean_absolute_error":
-            return mae.mean_absolute_error
+            return mean_absolute_error
         case "mean_squared_error":
-            return mse.mean_squared_error
+            return mean_squared_error
         case "l2_error":
-            return l2_error.l2_error
+            return l2_error
         case "l2_error_relative":
-            return l2_error_relative.l2_error_relative
+            return l2_error_relative
         case _:
             raise ValueError("Error function not defined")
 
@@ -91,10 +92,10 @@ def get_errors(y_true, y_pred):
     @returns {mae, mse, l2_error, l2_error_relative}
     """
     return {
-        'mean_absolute_error': mae.mean_absolute_error(y_true, y_pred),
-        'mean_squared_error': mse.mean_squared_error(y_true, y_pred),
-        'l2_error': l2_error.l2_error(y_true, y_pred),
-        'l2_error_relative': l2_error_relative.l2_error_relative(y_true, y_pred),
+        'mean_absolute_error': mean_absolute_error(y_true, y_pred),
+        'mean_squared_error': mean_squared_error(y_true, y_pred),
+        'l2_error': l2_error(y_true, y_pred),
+        'l2_error_relative': l2_error_relative(y_true, y_pred),
     }
 
 def get_results(experiment, model="ELM", dataset="train", type="errors", error_function="mean_absolute_error"):
@@ -102,7 +103,7 @@ def get_results(experiment, model="ELM", dataset="train", type="errors", error_f
     @param experiment : Experiment object consisting of multiple runs
     @param model      : "ELM", "U-SWIM", "A-SWIM", "SWIM"
     @param dataset    : "train", "test"
-    @param type       : "errors", "losses" where "errors" are calculated over the true and predicted H(x) values, "losses" are calculated over the true and predicted derivatives of H(x)
+    @param type       : "function_errors", "gradient_errors" where "function_errors" are calculated over the true and predicted H(x) values, "gradient_errors" are calculated over the true and predicted gradients of H(x)
     @param function   : "mean_absolute_error", "mean_squared_error", "l2_error", "l2_error_relative"
 
     @returns list of results
@@ -145,3 +146,28 @@ def get_summary(experiment, models, datasets, types, error_functions, stats):
 
                         summary.append(f'- {model}   \t: {str(error)}')
     return "\n".join(summary)
+
+def get_median_from_experiment(path_to_experiment, model_name="ELM", error_function="l2_error_relative", verbose=False):
+    """
+    helper to get median seed values from a saved experiment
+
+    @param model_name: "ELM", "USWIM", "ASWIM", "SWIM"
+    @param error_function: "l2_error_relative", "mean_absolute_error", "mean_squared_error", "l2_error"
+
+    @returns median_error, train_random_seed, test_random_seed, model
+    """
+    experiment = load(path_to_experiment)
+    if verbose:
+        print(f"-- Experiment Details --")
+        print(f"DOMAIN\n{experiment['domain_params']}")
+        print(f"{model_name}\n{experiment[model_name.lower().replace('-','') + '_params']}")
+    # errors = [ run['test_function_error'][model_name]['error_function'] for run in experiment['runs'] ]
+    errors = [ run['test_errors'][model_name][error_function] for run in experiment['runs'] ]
+    median_index = np.argsort(errors)[len(errors)//2]
+    median_run = experiment['runs'][median_index]
+
+    train_random_seed = median_run['train_random_seed']
+    test_random_seed = median_run['test_random_seed']
+    model = median_run[model_name]
+
+    return errors[median_index], train_random_seed, test_random_seed, model
