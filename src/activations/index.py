2c352f92872ec9b3325d076343dbfe25bb95288f
AlphaGergedan <atamertiel@gmail.com>
2024-05-11 18:16:13 +0200

    index for activations

---
+++ src/activations/index.py
@@ -0,0 +1,9 @@
+from .elu import elu, d_elu
+from .gelu import gelu, d_gelu
+from .relu import relu, d_relu, leaky_relu, d_leaky_relu, parametric_relu, d_parametric_relu
+from .silu import silu, d_silu
+from .tanh import tanh, d_tanh
+from .sigmoid import sigmoid, d_sigmoid
+from .identity import identity, d_identity
+from .gaussian import gaussian, d_gaussian
+from .softplus import softplus, d_softplus
